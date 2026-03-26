c4f2b4457ead92a42d3c4db8ea71c26f578dec09
igniskarya <karyaignis@gmail.com>
2026-03-27 04:35:21 +0700

    Update Navigation.h

--- firmware/boards/diy_smoochie/core/Navigation.h
+++ firmware/boards/diy_smoochie/core/Navigation.h
@@ -1,16 +1,54 @@
 #pragma once
 
-#include "core/INavigation.h"
-#include "core/IKeyboard.h"
+#include <Arduino.h>
+#include <functional>
 
-class NavigationImpl : public INavigation
-{
+enum NavigationAction {
+    NAV_SEL,
+    NAV_UP,
+    NAV_DW,
+    NAV_BACK,
+    NAV_NONE
+    };
+   
+
+enum NavigationMode {
+    MODE_DEFAULT,
+    MODE_ENCODER
+};
+
+class Navigation {
 public:
-  void begin() override {}
-
-  void update() override {
-  }
+    Navigation();
+    void begin();
+    void update();
+    NavigationAction getAction();
+    bool isPressed(NavigationAction action);
+    void setMode(NavigationMode mode);
+    NavigationMode getMode();
+    void resetMode(); // Hold select for 3 seconds to reset
 
 private:
-  Direction  _heldDir = DIR_NONE;
+    NavigationMode _mode;
+    NavigationAction _lastAction;
+    unsigned long _lastDebounceTime;
+    unsigned long _selectPressStart;
+    bool _selectHeld;
+    
+   /* GPIO pins for DIY Smoochie
+    static const uint8_t SEL_BTN = 0;
+    static const uint8_t UP_BTN = 41;
+    static const uint8_t DW_BTN = 40;*/
+    
+    // Button states
+    bool _selState;
+    bool _upState;
+    bool _dwState;
+    bool _lastSelState;
+    bool _lastUpState;
+    bool _lastDwState;
+    
+    void readButtons();
+    NavigationAction decodeAction();
+    void checkResetMode();
 };
